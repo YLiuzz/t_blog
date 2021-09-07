e9520fc5fb2a4bdc7c2ccd98a1b4ea8d17b44f19
Terry <tarry5508@gmail.com>
2021-09-07 17:40:07 +0800

    Update db setting

--- app/helpers.php
+++ app/helpers.php
@@ -5,7 +5,7 @@ function get_db_config()
     if(getenv('IS_IN_HEROKU')){
         $url = parse_url(getenv("DATABASE_URL"));
         return $db_config = [
-            'connection' => 'pqsql',
+            'connection' => 'pgsql',
             'host' => $url['host'],
             'database'  => substr($url['path'],1),
             'username'  => $url['user'],
